925fb2fc69b4615a174ad0eecfdf94683fb274e4
Gitalus <italog.penaloza@gmail.com>
2021-09-18 20:33:42 +0000

    create models for instagram-data-modeling

--- src/models.py
+++ src/models.py
@@ -1,32 +1,77 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Date
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class Profiles(Base):
+    __tablename__ = 'profiles'
+    # Here we define columns for the table profiles
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), primary_key=True)
+    avatar = Column(String(250))
+    presentacion = Column(Text)
     name = Column(String(250), nullable=False)
+    website= Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Publicaciones(Base):
+    __tablename__ = 'publicaciones'
+    # Here we define columns for the table publicaciones.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_publicaciones = Column(Integer, primary_key=True)
+    media = Column(String(250))
+    descripcion = Column(Text)
+    created_at = Column(Date(), server_default=func.now())
+    ubicacion = Column(String(250))
+    owner = Column(String(250), ForeignKey('profiles.user_id'))
 
-    def to_dict(self):
+
+class Comentarios(Base):
+    __tablename__ = 'comentarios'
+    # Here we define columns for the table comentarios.
+    # Notice that each column is also a normal Python instance attribute.
+    id_comentario = Column(Integer, primary_key=True)
+    owner_comentario = Column(String(250), ForeignKey('profiles.user_id'))
+    contenido = Column(Text)
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    # Here we define columns for the table likes.
+    # Notice that each column is also a normal Python instance attribute.
+    usuarios = Column(String(250), ForeignKey('profiles.user_id'), primary_key=True)
+    id_comentario = Column(Integer, ForeignKey('comentarios.id_comentario'), primary_key=True)
+    id_publicaciones = Column(Integer, ForeignKey('publicaciones.id_publicaciones'), primary_key=True)
+
+class Vistos(Base):
+    __tablename__ = 'vistos'
+    # Here we define columns for the table vistos.
+    # Notice that each column is also a normal Python instance attribute.
+    id_vistos = Column(Integer, primary_key=True)
+    usuarios = Column(String(250), ForeignKey('profiles.user_id'))
+
+class Historias(Base):
+    __tablename__ = 'historias'
+    # Here we define columns for the table historias.
+    # Notice that each column is also a normal Python instance attribute.
+    id_historias = Column(Integer, primary_key=True)
+    owner_historia = Column(String(250), ForeignKey('profiles.user_id'))
+    media = Column(String(250), nullable=False)
+    created_at = Column(Date(), server_default=func.now())
+    vistos_historias = Column(Integer, ForeignKey('vistos.id_vistos'))
+
+class Followers(Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table followers.
+    # Notice that each column is also a normal Python instance attribute.
+    user_from = Column(String(250), ForeignKey('profiles.user_id'), primary_key=True)
+    user_to = Column(String(250), ForeignKey('profiles.user_id'), primary_key=True)
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
